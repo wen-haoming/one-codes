161164396a6a3f64802ea92d622b2d177a79cf8f
haoming <435203093@qq.com>
2023-01-16 00:56:51 +0800

    chore: 更新react版本

--- src/components/Code/index.tsx
+++ src/components/Code/index.tsx
@@ -2,27 +2,27 @@ import { idSchema, schemaMap } from '@/store';
 import { parse } from '@/utils/parse';
 import * as monaco from 'monaco-editor';
 import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
-import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
-import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
-import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
-import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
+// import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
+// import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
+// import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
+// import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
 import { useEffect, useRef } from 'react';
 import { useSnapshot } from 'valtio';
 
 self.MonacoEnvironment = {
   getWorker(_, label) {
-    if (label === 'json') {
-      return new jsonWorker();
-    }
-    if (label === 'css' || label === 'scss' || label === 'less') {
-      return new cssWorker();
-    }
-    if (label === 'html' || label === 'handlebars' || label === 'razor') {
-      return new htmlWorker();
-    }
-    if (label === 'typescript' || label === 'javascript') {
-      return new tsWorker();
-    }
+    // if (label === 'json') {
+    //   return new jsonWorker();
+    // }
+    // if (label === 'css' || label === 'scss' || label === 'less') {
+    //   return new cssWorker();
+    // }
+    // if (label === 'html' || label === 'handlebars' || label === 'razor') {
+    //   return new htmlWorker();
+    // }
+    // if (label === 'typescript' || label === 'javascript') {
+    //   return new tsWorker();
+    // }
     return new editorWorker();
   },
 };
