a0ab87d424e473c8b56d22f00860742776f4bc82
haoming <435203093@qq.com>
2023-01-30 02:00:16 +0800

    fix: 优化部分代码

--- src/Layout/RenderPanel.tsx
+++ src/Layout/RenderPanel.tsx
@@ -1,23 +1,9 @@
 import Code from '@/components/Code';
 import Render from '@/components/Render';
 import { currentState } from '@/store';
-import { FileCode, Workbench } from '@icon-park/react';
-import { Space } from 'antd';
+import { FileTextOutlined, FormOutlined } from '@ant-design/icons';
+import { Button, Space, Tooltip } from 'antd';
 import { useSnapshot } from 'valtio';
-
-const modeList: {
-  type: 'edit' | 'code';
-  Icon: any;
-}[] = [
-  {
-    type: 'edit',
-    Icon: Workbench,
-  },
-  {
-    type: 'code',
-    Icon: FileCode,
-  },
-];
 
 const RenderPanel = () => {
   const currentStateSnap = useSnapshot(currentState);
@@ -25,25 +11,24 @@ const RenderPanel = () => {
   return (
     <div className="flex flex-col flex-1 bg-brand-grey p-t-0">
       <Space className="flex h-7 w-full bg-white b-brand-grey border-l-1 border-r-1 justify-end items-center p-x2">
-        {modeList.map((item, key) => {
-          const { Icon } = item;
-          // eslint-disable-next-line react/jsx-key
-          return (
-            <Icon
-              key={key}
-              className="cursor-pointer"
-              onClick={() => {
-                currentState.panelMode = item.type;
-              }}
-              theme="outline"
-              size="16"
-              fill={
-                currentStateSnap.panelMode === item.type ? '#2558fb' : '#9b9b9b'
-              }
-              strokeLinejoin="miter"
-            />
-          );
-        })}
+        <Tooltip title="编辑模式">
+          <Button size="small" type="text"
+            onClick={() => {
+              currentState.panelMode = 'edit'
+            }}
+            style={{
+              color: currentStateSnap.panelMode === 'edit' ? '#2558fb' : '#9b9b9b'
+            }} icon={<FormOutlined />} />
+        </Tooltip>
+        <Tooltip title="代码查看模式">
+          <Button size="small" type="text"
+            onClick={() => {
+              currentState.panelMode = 'code'
+            }}
+            style={{
+              color: currentStateSnap.panelMode === 'code' ? '#2558fb' : '#9b9b9b'
+            }} icon={<FileTextOutlined />} />
+        </Tooltip>
       </Space>
       <div className="flex flex-col h-[calc(100vh-5rem)] bg-white overflow-y-auto m-2 p-1 flex-col">
         {currentStateSnap.panelMode === 'edit' && <Render />}
