1966eefcaa778208991a4a6e39730cc69d2dd4f2
haoming <435203093@qq.com>
2023-02-09 22:41:54 +0800

    feat: 还原代码

--- src/layout/RenderPanel.tsx
+++ src/layout/RenderPanel.tsx
@@ -1,12 +1,9 @@
 import Code from '@/components/Code';
-// import Render from '@/components/Render';
 import { currentState } from '@/store';
 import { FileTextOutlined, FormOutlined } from '@ant-design/icons';
 import { Button, Space, Tooltip } from 'antd';
 import { useSnapshot } from 'valtio';
 import Render from '@/components/Render';
-import srcDoc from '../dist/index.js?raw' // prod
-import antcss from 'antd/dist/antd.css?raw' // prod
 
 const RenderPanel = () => {
   const currentStateSnap = useSnapshot(currentState);
@@ -34,12 +31,7 @@ const RenderPanel = () => {
         </Tooltip>
       </Space>
       <div className="h-[calc(100vh-5rem)] bg-white overflow-y-auto m-2 p-1 flex-col">
-        {currentStateSnap.panelMode === 'edit' && <iframe className='h-100% h-100%' width={'100%'} src={import.meta.env.DEV ? '/one-codes/src/render/index.html' : undefined} srcDoc={import.meta.env.PROD ? 
-          `<style>${antcss}</style>
-          <div id="root"></div>
-          <script type="module">${srcDoc}</script>`
-           : undefined} />}
-        {/* {currentStateSnap.panelMode === 'edit' && <Render />} */}
+        {currentStateSnap.panelMode === 'edit' && <Render />}
         {currentStateSnap.panelMode === 'code' && <Code />}
       </div>
     </div>
